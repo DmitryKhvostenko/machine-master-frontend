2ef50c820f042aa6c100bf49336fb3d981b1971a
DmitryKhvostenko <gerawgha@gmail.com>
2024-07-22 20:45:18 +0300

    added routes
    created route for machines

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { MachinesComponent } from './pages/machines/machines.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'machines', pathMatch: 'full' },
+
+	{ path: 'machines', component: MachinesComponent },
+
+	{ path: '**', redirectTo: 'machines', pathMatch: 'full' },
+]
